1848fe846551738472efecbb38dc98165a4b1598
J-bot-oss <jesseebong656@gmail.com>
2025-08-16 22:13:10 +0300

    Create variables.py
    One thing about variables is that they are case-sensitive meaning that 'message' is different from 'Message'

---
+++ variables.py
@@ -0,0 +1,21 @@
+#variables are containers that store data values
+# A vairable can be an integer, float, string or boolean(these are th differnt types)
+
+#Take for example:
+message = "Hello, World!" # This is a string value stored in a variable called message.
+
+number = 42 # This is an integer value stored in a variable called number.
+
+pi = 3.14 # This is a float value stored ian variable clled pi.
+
+is_active = True # This is a boolean value stored in a variable called is_active.
+
+
+#To print the values of the variables inilalized above, we use the print function:
+print(message) # Output: Hello, World!
+
+print(number) # Output: 42
+
+print(pi) # Output: 3.14
+
+print(is_active) # Output: True
