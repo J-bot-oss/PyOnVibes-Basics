b48c7eb2815802a517c14e2a7ace70779a101b3c
J-bot-oss <jesseebong656@gmail.com>
2025-08-17 10:40:11 +0300

    Create strings.py
    This is the introductory part of strings which is one of the data types

---
+++ strings.py
@@ -0,0 +1,29 @@
+# A string is a series of characters enclosed in quotes.
+# It can either be single quoted or double quoted.
+
+# for example:
+greeting = 'Hello, World!' # This is a string value stored ina variable called greeting
+greeting_double = "Hello, World!" # This is also a string value stored in a variable called greeting_double
+
+# The quotes also allow for flexibility in how we can define strings.
+# We can use both single quotes and double quotes.
+
+# For example:
+message = 'She said "Hello!"' # This string has double quotes inside single quotes.
+message_double = "The language 'Python' is named after Monty Python." # This string has single quotes inside double quotes
+
+# We can also use triple quotes for multi-line strings.
+multi_line_message = """This is a multi-line string""" # or
+multi_line_message_triple = '''This is  also a multi-line string''' # both are valid
+
+# To print the values of the strings initialized above, we use the print function:
+print(greeting) # output: Hello, World!
+print(greeting_double) # ouput: Hello, World!
+print(message) # Output: She said "Hello!"
+print(message_double) # Output: The language 'Python' is named after Monty Python.
+print(multi_line_message) # Output: This is a multi-line string.
+print(multi_line_message_triple) # Output: This is also a multi-line string.
+
+
+
+
