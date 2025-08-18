5099dbac98f5d3c0b68332e2e514d0f258b2e7c8
J-bot-oss <jesseebong656@gmail.com>
2025-08-18 08:30:43 +0300

    Create methods_string.py
    Methods just help you do a lot of  things with strings

---
+++ methods_string.py
@@ -0,0 +1,12 @@
+#Methods are actions that can be performed on strings.
+# They allow  us to manipulate string data in various ways.
+
+name = "ada lovelace" 
+print(name.upper()) # this converts the string to uppercase
+
+name = "Alice"
+print(name.lower()) # this converts the string to lowercase
+
+name = "jesse johnson"
+print(name.title()) #this converts the first letter of each word to uppercase
+
